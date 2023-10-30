5f231a22f743956eac130602dbb106e3ed7f3b95
Ram Barada <rambarada123@gmail.com>
2023-10-30 11:53:46 +0200

    feat(element): add material toolbar to the header component and a button to trigger the add-edit contact dialog component

--- contact-app/contact-app/src/app/components/header/header.component.ts
+++ contact-app/contact-app/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddEditContactComponent } from '../add-edit-contact/add-edit-contact.component';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+
+  constructor(private _dialog: MatDialog){}
+
+  onAddEditContactform() {
+    this._dialog.open(AddEditContactComponent);
+  }
 }
